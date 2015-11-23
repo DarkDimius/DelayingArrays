e54cdfbf3002ec524799e9f43cff02a6d2777e07
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2015-11-23 23:59:07 +0100

    Add vector to comparison.

--- src/main/scala/me/d_d/delaying/Benchmarks.scala
+++ src/main/scala/me/d_d/delaying/Benchmarks.scala
@@ -3,6 +3,7 @@ package me.d_d.delaying
 import java.util.concurrent.TimeUnit
 
 import org.openjdk.jmh.annotations._
+import org.openjdk.jmh.infra.Blackhole
 
 /**
   * Created by dark on 23/11/15.
@@ -12,19 +13,27 @@ class Benchmarks {
 
 
   val size = 73121
-  val darr = DArray(1 to 73121 :_*)
-  val arr = Array(1 to 73121 :_*)
+  val darr = DArray(1 to size :_*)
+  val arr = Array(1 to size :_*)
+  val vec = Vector(1 to size : _*)
   val rnd = new java.util.Random(42)
 
 
   @Benchmark
   @BenchmarkMode(Array(/*Mode.Throughput, */Mode.AverageTime/*, Mode.SampleTime, Mode.SingleShotTime*/))
   @OutputTimeUnit(TimeUnit.NANOSECONDS)
-  def darr_apply = darr.apply(rnd.nextInt(size))
+  def darr_apply(bh: Blackhole) = darr.apply(rnd.nextInt(size))
 
   @Benchmark
   @BenchmarkMode(Array(/*Mode.Throughput, */Mode.AverageTime/*, Mode.SampleTime, Mode.SingleShotTime*/))
   @OutputTimeUnit(TimeUnit.NANOSECONDS)
-  def arr_apply = arr.apply(rnd.nextInt(size))
+  def arr_apply(bh: Blackhole) = arr.apply(rnd.nextInt(size))
+
+  @Benchmark
+  @BenchmarkMode(Array(/*Mode.Throughput, */Mode.AverageTime/*, Mode.SampleTime, Mode.SingleShotTime*/))
+  @OutputTimeUnit(TimeUnit.NANOSECONDS)
+  def vec_apply(bh: Blackhole) = vec.apply(rnd.nextInt(size))
+
+
 
 }
