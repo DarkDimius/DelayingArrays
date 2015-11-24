bf5032faa6091d0b5a40988579ad73ec8b1237c7
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2015-11-24 02:05:18 +0100

    Add benchmark output.

--- src/main/scala/me/d_d/delaying/Benchmarks.scala
+++ src/main/scala/me/d_d/delaying/Benchmarks.scala
@@ -11,6 +11,30 @@ import org.openjdk.jmh.infra.Blackhole
 @State(Scope.Benchmark)
 class Benchmarks {
 
+  /*
+[info] Benchmark                         Mode  Cnt       Score        Error  Units
+[info] Benchmarks.arr_apply              avgt   30      13.495 ±      5.990  ns/op
+[info] Benchmarks.darr_apply             avgt   30      22.800 ±      6.680  ns/op
+[info] Benchmarks.vec_apply              avgt   30      28.919 ±     38.852  ns/op
+
+[info] Benchmarks.arr_foreach            avgt   30  423603.350 ± 239141.019  ns/op
+[info] Benchmarks.darr_foreach           avgt   30  240191.217 ±  51075.970  ns/op
+[info] Benchmarks.vec_foreach            avgt   30  299815.300 ±  74171.103  ns/op
+
+[info] Benchmarks.arr_iterator_foreach   avgt   30  448778.639 ± 176362.034  ns/op
+[info] Benchmarks.darr_iterator_foreach  avgt   30  643554.768 ±  95530.380  ns/op
+[info] Benchmarks.vec_iterator_foreach   avgt   30  306579.976 ± 324088.290  ns/op
+    */
+
+  // as can be seen from numbers above:
+  // 1: for apply: faster then vector, and substantially less deviation
+  //      profiles suggest that hot place is calculation of indexes in arr_apply
+  //      I was already able to speed it up by 20% by adding more byte-shift a trick there
+  //      it would be good to see if some more tricks could be done to reduce number of iterations
+  //      would be even better to find a way to not have a cycle in the first place
+  //
+  // 2: for foreach and iterator: reboxing spoils the picture
+
 
   val size = 73121
   val darr = DArray(1 to size :_*)
