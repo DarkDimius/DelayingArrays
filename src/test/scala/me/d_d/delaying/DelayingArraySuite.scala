3d4cac9343608e4a75fc74b795b1a62f35355771
Alfonso2 Peterssen <a2peterssen@gmail.com>
2016-04-01 18:59:28 +0200

    Add simple correctness tests

--- src/test/scala/me/d_d/delaying/DelayingArraySuite.scala
+++ src/test/scala/me/d_d/delaying/DelayingArraySuite.scala
@@ -1,8 +1,55 @@
 package me.d_d.delaying
 
-/**
-  * Created by mukel on 4/1/16.
-  */
-class DelayingArraySuite {
+import org.scalatest._
 
+import scala.collection.mutable.ArrayBuffer
+
+class DelayingArraySuite extends FunSuite with Matchers {
+
+  val sizes = Array(0, 1, 2, 6, 7, 8, 10, 24, 35, 60, 100, 250, 310, 511, 512, 1025, 12345678)
+
+  test("apply") {
+    for (size <- sizes) {
+      val range = 0 until size
+      val darr = DelayingArray(range: _*)
+      assert(darr.size === size)
+      for (i <- range)
+        assert(i === darr.apply(i))
+    }
+  }
+
+  test("append") {
+    for (size <- sizes) {
+      val range = 0 until size
+      val darr = range.foldLeft(DelayingArray.empty)(_ append _)
+      assert(darr.size === size)
+      for (i <- range)
+        assert(i === darr.apply(i))
+    }
+  }
+
+  test("foreach traverses all elements") {
+    for (size <- sizes) {
+      val range = 0 until size
+      val darr = DelayingArray(range: _*)
+
+      var sum = 0L
+      darr.foreach(sum += _)
+      val targetSum = (0L /: range)(_ + _)
+
+      assert(sum === targetSum)
+    }
+  }
+
+  test("foreach respects sequential order") {
+    for (size <- sizes) {
+      val range = 0 until size
+      val darr = DelayingArray(range: _*)
+
+      val buf = ArrayBuffer[Int]()
+      darr.foreach(buf += _)
+
+      range should equal (buf)
+    }
+  }
 }
